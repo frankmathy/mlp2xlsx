c12c5902de1ca5c507db16e0bfecbda486dc8a9d
Frank Mathy <frankmathy@gmail.com>
2024-01-03 01:09:01 +0100

    Finished implementation

--- xlsx-collector.js
+++ xlsx-collector.js
@@ -1,7 +1,7 @@
-// Playlist converter for mAirList playlist files to Excel
-// (c) frankmathy@gmail.com, 2022
-const xml2js = require("xml2js");
+// Read Excel Playlists and create one single Playlist with dates
+// (c) frankmathy@gmail.com, 2024
 const xlsx = require("xlsx");
+const nodeXlsx = require("node-xlsx");
 const fs = require("fs");
 const path = require("path");
 var xl = require("excel4node");
@@ -33,6 +33,8 @@ if (process.argv.length < 4) {
 const directoryName = process.argv[2];
 const excelFileName = process.argv[3];
 
+var allSongs = [["Artist", "Title", "Date"]];
+
 const files = fs.readdirSync(directoryName, {
   recursive: true,
 });
@@ -40,7 +42,6 @@ const xlsxFiles = files.filter((file) => file.toLowerCase().endsWith(".xlsx"));
 xlsxFiles.forEach((xlsFileName) => {
   if (xlsFileName.indexOf("~") < 0) {
     const xlsPathName = path.join(directoryName, xlsFileName);
-    console.log(xlsPathName);
     const file = xlsx.readFile(xlsPathName);
     const sheets = file.SheetNames;
     if (sheets.length > 0) {
@@ -48,48 +49,11 @@ xlsxFiles.forEach((xlsFileName) => {
       const filteredTrackData = trackData
         .filter((line) => isValidTrackEntry(line[0]))
         .map((filteredLine) => [filteredLine[0], filteredLine[1], getDateFromFileName(xlsFileName)]);
-      console.log(JSON.stringify(filteredTrackData));
+      console.log(`${xlsPathName} => ${filteredTrackData.length} songs`);
+      allSongs = allSongs.concat(filteredTrackData);
     }
   }
 });
 
-/*
-var wb = new xl.Workbook();
-var ws = wb.addWorksheet("Songs");
-ws.cell(1, 1).string("Playlist: ");
-let row = 3;
-ws.cell(row, 1).string("Artist");
-ws.cell(row, 2).string("Title");
-ws.cell(row++, 3).string("Artist - Title");
-
-mlpFiles.forEach((fileName) => {
-  var songCount = 0;
-
-  // read XML from a file
-  const xml = fs.readFileSync(fileName);
-
-  // convert XML to JSON
-  xml2js.parseString(xml, { mergeAttrs: true }, (err, data) => {
-    if (err) {
-      throw err;
-    }
-    data.Playlist.PlaylistItem.forEach((item) => {
-      if (item.Type !== undefined) {
-        const [itemType] = item.Type;
-        if (itemType === "Music") {
-          const [artist] = item.Artist;
-          const [title] = item.Title;
-          ws.cell(row, 1).string(artist);
-          ws.cell(row, 2).string(title);
-          ws.cell(row++, 3).string(artist + " - " + title);
-          songCount++;
-        }
-      }
-    });
-  });
-  console.log(`${songCount} songs in ${fileName}`);
-});
-
-wb.write(excelFileName);
-console.log(`Playlist written to ${excelFileName}`);
-*/
+const xlsXBuffer = nodeXlsx.build([{ name: " Playlists Songs", data: allSongs }]);
+fs.writeFileSync(excelFileName, xlsXBuffer);
